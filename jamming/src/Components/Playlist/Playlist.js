db706f5351e7358239416a74ab2a33127596e9cf
Adam Siegel <siegelz@sbcglobal.net>
2019-04-04 14:09:24 -0700

    Change the Name of a Playlist_Final

--- jamming/src/Components/Playlist/Playlist.js
+++ jamming/src/Components/Playlist/Playlist.js
@@ -6,10 +6,23 @@ import TrackList from './TrackList/TrackList';
 
 
 class Playlist extends React.Component {
+constructor(props) {
+  super(props);
+
+  this.handleNameChange = this.handleNameChange.bind(this);
+}
+
+handleNameChange(event) {
+  this.props.onNameChange(event.target.value);
+}
+
+
+
+
   render () {
     return (
 <div className="Playlist">
-  <input defaultValue={'New Playlist'}/>
+  <input defaultValue={'New Playlist'} onChange={this.handleNameChange} />
   <Tracklist tracks={this.props.playlistTracks} onRemove={this.props.onRemove} />
   <a class="Playlist-save">SAVE TO SPOTIFY</a>
 </div>
