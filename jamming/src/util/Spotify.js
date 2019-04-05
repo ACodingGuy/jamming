43e8e5b34c72507a81e0ce086cc658ba3a928825
Adam Siegel <siegelz@sbcglobal.net>
2019-04-05 16:26:42 -0700

    Obtain a Spotify Access Token_Final

--- jamming/src/util/Spotify.js
+++ jamming/src/util/Spotify.js
@@ -7,8 +7,19 @@ const Spotify = {
     if(accessToken) {
       return accessToken;
     }
+const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+const expirationTimeMatch = window.location.href.match(/expires_in=([^&]*)/);
+    if (accessTokenMatch && expirationTimeMatch)  {
+      accessToken = accessTokenMatch[1];
+      expirationTime = Number(expirationTimeMatch[1]);
+      window.setTimeout(() => accessToken = '', expirationTime * 1000);
+      window.history.pushState('Access Token', null, '/');
+      return accessToken;
+  } else {
+    let accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+    window.location = accessUrl;
   }
-}
+},
 
 
 
